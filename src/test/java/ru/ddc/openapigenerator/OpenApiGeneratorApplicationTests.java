aa659166f19c5c163b9fe37efd3aaeb4486da525
Sergey Kovalev <dipdeepcode@mail.ru>
2024-11-01 19:36:33 +0300

    feat :: init commit

---
+++ src/test/java/ru/ddc/openapigenerator/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.openapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
